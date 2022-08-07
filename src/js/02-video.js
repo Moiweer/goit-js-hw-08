b27e83351f3a3144ed13375851f9f2adc4d7391b
Mariya Barska <moiweer@gmail.com>
2022-08-07 15:57:50 +0300

    hw8 part 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+ const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+function onCurrentTime(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime)
+    console.log(currentTime);
+}
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+    .setCurrentTime(getCurrentTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+            default:
+            break;
+    }
+});
