9e2572cfa912206e23ce5e82981060dd7ea76c57
Mariya Barska <moiweer@gmail.com>
2022-08-18 15:33:03 +0300

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,58 @@
 
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state'
+
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     textarea: document.querySelector('.feedback-form textarea'),
+//     emailElement: document.querySelector('.feedback-form input'),
+// };
+// populateTextarea();
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+// refs.textarea.addEventListener('textarea', throttle(onTextareaInput, 500));
+
+
+// function onTextareaInput(evt) {
+//         evt.preventDefault();
+
+//     let formData = localStorage.getItem(STORAGE_KEY)
+//         ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+//         : {};
+//     formData[evt.target.name] = evt.target.name.value;
+
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+//     const email = evt.target.elements.email.value;
+//     const message = evt.target.elements.message.value;
+    
+//     if (email === '' || message === '') {
+//         alert('Заповни всі поля!')
+//     } else {
+//     localStorage.removeItem(STORAGE_KEY);   
+//         console.log({ email, message });
+// }
+//     evt.currentTarget.reset()
+// }
+
+
+// function populateTextarea() {
+//     let formData = localStorage.getItem(STORAGE_KEY);
+//     if (formData) {
+//         formData = JSON.parse(formData);
+//         Object.entries(formData).forEach(([name, value]) => {
+//             refs.form.elements[name].value = value;
+//         })
+      
+//     }
+// }
+// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
+
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state'
@@ -6,13 +60,13 @@ const STORAGE_KEY = 'feedback-form-state'
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
-    emailElement: document.querySelector('input'),
+    emailElement: document.querySelector('.feedback-form input'),
 };
 populateTextarea();
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
+refs.textarea.addEventListener('textarea', throttle(onTextareaInput, 500));
 
 
 function onTextareaInput(evt) {
@@ -21,7 +75,7 @@ function onTextareaInput(evt) {
     let formData = localStorage.getItem(STORAGE_KEY)
         ? JSON.parse(localStorage.getItem(STORAGE_KEY))
         : {};
-    formData[evt.target.name] = evt.target.value;
+    formData[evt.target.name] = evt.target.name.value;
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
