6d2d3e41cb92b5f3b0dd4f7353a0eacc6dae82d2
Mariya Barska <moiweer@gmail.com>
2022-08-30 19:22:16 +0300

    new localStorage
    запис в локальне сховище данних з форми, збереження, та очищення форми та локального сховища після сабміту

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,93 +1,27 @@
 
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state'
-
-// const refs = {
-//     form: document.querySelector('.feedback-form'),
-//     textarea: document.querySelector('.feedback-form textarea'),
-//     emailElement: document.querySelector('.feedback-form input'),
-// };
-// populateTextarea();
-
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.textarea.addEventListener('textarea', throttle(onTextareaInput, 500));
-
-
-// function onTextareaInput(evt) {
-//         evt.preventDefault();
-
-//     let formData = localStorage.getItem(STORAGE_KEY)
-//         ? JSON.parse(localStorage.getItem(STORAGE_KEY))
-//         : {};
-//     formData[evt.target.name] = evt.target.name.value;
-
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
-//     const email = evt.target.elements.email.value;
-//     const message = evt.target.elements.message.value;
-    
-//     if (email === '' || message === '') {
-//         alert('Заповни всі поля!')
-//     } else {
-//     localStorage.removeItem(STORAGE_KEY);   
-//         console.log({ email, message });
-// }
-//     evt.currentTarget.reset()
-// }
-
-
-// function populateTextarea() {
-//     let formData = localStorage.getItem(STORAGE_KEY);
-//     if (formData) {
-//         formData = JSON.parse(formData);
-//         Object.entries(formData).forEach(([name, value]) => {
-//             refs.form.elements[name].value = value;
-//         })
-      
-//     }
-// }
-// ----------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------
-
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state'
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-    emailElement: document.querySelector('.feedback-form input'),
-};
+
+const form = document.querySelector('.feedback-form');
+
+let formObject = {};
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
 populateTextarea();
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-refs.textarea.addEventListener('textarea', throttle(onTextareaInput, 500));
-
-
 function onTextareaInput(evt) {
-        evt.preventDefault();
+    evt.preventDefault();
+    
+    formObject[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject));
 
-    let formData = localStorage.getItem(STORAGE_KEY)
-        ? JSON.parse(localStorage.getItem(STORAGE_KEY))
-        : {};
-    formData[evt.target.name] = evt.target.name.value;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-function populateTextarea() {
-    let formData = localStorage.getItem(STORAGE_KEY);
-    if (formData) {
-        formData = JSON.parse(formData);
-        Object.entries(formData).forEach(([name, value]) => {
-            refs.form.elements[name].value = value;
-        })
-      
-    }
+
 function onFormSubmit(evt) {
     evt.preventDefault();
     const email = evt.target.elements.email.value;
@@ -103,7 +37,14 @@ function onFormSubmit(evt) {
 }
 
 
-
+function populateTextarea() {
+    let formData = localStorage.getItem(STORAGE_KEY);
+    if (formData) {
+        formData = JSON.parse(formData);
+        Object.entries(formData).forEach(([name, value]) => {
+            form.elements[name].value = value;
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+        })
       
-    
+    }
 }
